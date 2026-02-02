70ad9ba7f01131167fbc0e68fa3f64909c9c829e
aknoss <pedrorfig27@gmail.com>
2026-02-02 20:08:06 -0300

    add index and signup pages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import path from "path";
+import type { Request, Response } from "express";
 
 const PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+app.get("/signup", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "..", "public", "signup.html"));
 });
 
 app.listen(PORT, () => {
