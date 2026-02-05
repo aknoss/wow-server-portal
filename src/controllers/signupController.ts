5ab57dbc4b45749f30253544c85d9d3d7d174a59
aknoss <pedrorfig27@gmail.com>
2026-02-05 18:11:19 -0300

    add create account but not working

--- src/controllers/signupController.ts
+++ src/controllers/signupController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import crypto from "crypto";
+import bigInt from "big-integer";
 import { pool } from "../db";
 import { RowDataPacket } from "mysql2";
 
@@ -7,6 +9,29 @@ const MAX_LENGTH = 32;
 
 function renderError(res: Response, error: string) {
   res.render("signup", { error });
+}
+
+const N = bigInt(
+  "894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7",
+  16,
+);
+const g = bigInt(7);
+
+function sha1(buffer: Buffer) {
+  return crypto.createHash("sha1").update(buffer).digest();
+}
+
+function generateVerifier(username: string, password: string, salt: Buffer) {
+  const upUser = username.toUpperCase();
+  const upPass = password.toUpperCase();
+
+  const h1 = sha1(Buffer.from(`${upUser}:${upPass}`));
+  const h2 = sha1(Buffer.concat([salt, h1]));
+
+  const x = bigInt.fromArray([...h2].reverse(), 256);
+  const v = g.modPow(x, N);
+
+  return Buffer.from(v.toArray(256).value.reverse());
 }
 
 export async function signupController(req: Request, res: Response) {
@@ -24,6 +49,14 @@ export async function signupController(req: Request, res: Response) {
   if (!password) {
     renderError(res, "Missing password.");
     return;
+  }
+
+  if (username.includes(" ")) {
+    return renderError(res, "Username must not contain spaces.");
+  }
+
+  if (password.includes(" ")) {
+    return renderError(res, "Password must not contain spaces.");
   }
 
   if (username.length < MIN_LENGTH) {
@@ -46,6 +79,7 @@ export async function signupController(req: Request, res: Response) {
     return;
   }
 
+  // Check if user already exists
   try {
     const [rows] = await pool.query<RowDataPacket[]>(
       `SELECT 1 FROM account WHERE username = ? LIMIT 1;`,
@@ -60,5 +94,23 @@ export async function signupController(req: Request, res: Response) {
     console.error(error);
     renderError(res, "Something went wrong.");
     return;
+  }
+
+  const salt = crypto.randomBytes(32);
+  const verifier = generateVerifier(username, password, salt);
+  const saltHex = salt.toString("hex");
+  const verifierHex = verifier.toString("hex");
+
+  try {
+    await pool.query(`INSERT INTO account (username, s, v) VALUES (?, ?, ?)`, [
+      username.toUpperCase(),
+      saltHex,
+      verifierHex,
+    ]);
+
+    res.render("signup-success", { username });
+  } catch (error) {
+    console.error(error);
+    renderError(res, "Something went wrong.");
   }
 }
