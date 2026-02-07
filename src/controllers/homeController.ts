e0354eff2be4bb2466deee802fba0804784ad932
aknoss <pedrorfig27@gmail.com>
2026-02-07 12:44:26 -0300

    cache on home page

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,19 +2,31 @@ import { Request, Response } from "express";
 import { pool } from "../db";
 import { RowDataPacket } from "mysql2";
 
+let cachedIsOnline: boolean | null = null;
+let cachedAt = 0;
+const TTL = 30000; // 30s
+
 export async function homeController(_req: Request, res: Response) {
-  let isOnline: boolean | null = null;
+  // Client cache of 30 seconds
+  res.set("Cache-Control", "public, max-age=30");
+
+  const now = Date.now();
+
+  // Server cache of 30 seconds
+  if (cachedIsOnline !== null && now - cachedAt < TTL) {
+    res.render("index", { isOnline: cachedIsOnline });
+    return;
+  }
 
   try {
     const [rows] = await pool.query<RowDataPacket[]>(
       "SELECT 1 FROM tbccharacters.characters WHERE online = 1 LIMIT 1;",
     );
-    isOnline = rows.length > 0;
+    cachedIsOnline = rows.length > 0;
+    cachedAt = now;
   } catch (error) {
     console.error(error);
   }
 
-  // Client cache of 30 seconds
-  res.set("Cache-Control", "public, max-age=30");
-  res.render("index", { isOnline });
+  res.render("index", { isOnline: cachedIsOnline });
 }
